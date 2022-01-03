a2d93d27cb2df12a423543966421cdf16f19197f
jens <android-development@gmx.de>
2022-01-03 19:50:57 +0100

    Cosmetics

--- app/src/main/java/com/jens/automation2/ActivityPermissions.java
+++ app/src/main/java/com/jens/automation2/ActivityPermissions.java
@@ -3,7 +3,6 @@ package com.jens.automation2;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
@@ -21,9 +20,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.core.app.NotificationManagerCompat;
 
 import com.jens.automation2.receivers.NotificationListener;
 
