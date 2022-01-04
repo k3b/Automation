8c6331237d7e86ea5b53b7b13588497cd1c305ec
jens <android-development@gmx.de>
2022-01-04 16:14:13 +0100

    Cosmetics

--- app/src/main/java/com/jens/automation2/receivers/PackageReplacedReceiver.java
+++ app/src/main/java/com/jens/automation2/receivers/PackageReplacedReceiver.java
@@ -14,29 +14,18 @@ public class PackageReplacedReceiver extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-//		Toast.makeText(context, "package replaced", Toast.LENGTH_LONG).show();
-//		int intentUid = intent.getExtras().getInt("android.intent.extra.UID");	// userid of the application that has just been updated
-//		int myUid = android.os.Process.myUid();										// userid of this application
-//
-//		boolean replacing = intent.getBooleanExtra(Intent.EXTRA_REPLACING, false);
-		
-//		if(intentUid == myUid)
-//		{
-			Settings.readFromPersistentStorage(context);
+		Settings.readFromPersistentStorage(context);
 
-			Miscellaneous.logEvent("i", context.getResources().getString(R.string.applicationHasBeenUpdated), context.getResources().getString(R.string.applicationHasBeenUpdated), 2);
-			if(hasServiceBeenRunning() && Settings.startServiceAfterAppUpdate)
-			{
-				Miscellaneous.logEvent("i", "Service", context.getResources().getString(R.string.logStartingServiceAfterAppUpdate), 1);
-				AutomationService.startAutomationService(context, true);
-			}
-			else
-			{
-				Miscellaneous.logEvent("i", "Service", context.getResources().getString(R.string.logNotStartingServiceAfterAppUpdate), 2);
-			}
-//		}
-//		else
-//			Miscellaneous.logEvent("i", "Service", "Some other app has been updated.", 5);
+		Miscellaneous.logEvent("i", context.getResources().getString(R.string.applicationHasBeenUpdated), context.getResources().getString(R.string.applicationHasBeenUpdated), 2);
+		if(hasServiceBeenRunning() && Settings.startServiceAfterAppUpdate)
+		{
+			Miscellaneous.logEvent("i", "Service", context.getResources().getString(R.string.logStartingServiceAfterAppUpdate), 1);
+			AutomationService.startAutomationService(context, true);
+		}
+		else
+		{
+			Miscellaneous.logEvent("i", "Service", context.getResources().getString(R.string.logNotStartingServiceAfterAppUpdate), 2);
+		}
 	}
 	
 	private static boolean hasServiceBeenRunning()
