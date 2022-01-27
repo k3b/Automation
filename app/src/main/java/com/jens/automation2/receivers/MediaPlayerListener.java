5ed024774eeda5279cf867fc201e0e0af3c95876
Jens <android-development@gmx.de>
2022-01-27 23:05:54 +0100

    Control media playback

---
+++ app/src/main/java/com/jens/automation2/receivers/MediaPlayerListener.java
@@ -0,0 +1,11 @@
+package com.jens.automation2.receivers;
+
+import android.media.MediaPlayer;
+
+public class MediaPlayerListener
+{
+    void test()
+    {
+        MediaPlayer.OnInfoListener
+    }
+}
